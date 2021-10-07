fb261eecd4d944b26025c6bb53c9de641bf26655
mrfaizanshariff <mrfaizanshariff@gmail.com>
2021-10-07 08:18:34 +0530

    first Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-gen';
+
+  length=0
+  password=''
+  includeLetters=false
+  includeNumbers=false
+  includeSymbols=false
+
+  onChangeLength(event:any){
+    const parsedVal = parseInt(event.target.value)
+    if(!isNaN(parsedVal)){
+      this.length=parsedVal
+    }
+  }
+  onChangeUseNumbers(){
+    this.includeNumbers=!this.includeNumbers
+  }
+  onChangeUseSymbols(){
+    this.includeSymbols=!this.includeSymbols
+  }
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters
+  }
+  onButtonClick(){
+  // prgrm to randomly generate password
+
+  const number = '1234567890'
+  const letters='abcdefghijklmnopqrstuvwxyz'
+  const symbols = '!@#$%^&*()'
+
+  let validChars = ''
+  if(this.includeNumbers){
+    validChars+=number
+  }
+  if(this.includeLetters){
+    validChars+=letters
+  }
+  if(this.includeSymbols){
+    validChars+=symbols
+  }
+
+  let generatedPassword = ''
+  for(let i=0;i<this.length; i++){
+
+    // generate a random nmbr
+    const index = Math.floor(Math.random()* validChars.length)
+    generatedPassword+=validChars[index]
+
+  }
+
+  this.password=generatedPassword
+  console.log(this.password);
 }
+
+
+
+}
+
